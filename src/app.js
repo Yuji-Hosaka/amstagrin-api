32407db622331d83ce5fea98819fad3ce1cdb64a
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-10 15:49:24 +0700

    config app.js, add not-found middleware, express rate-limit

--- src/app.js
+++ src/app.js
@@ -5,12 +5,19 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const notFoundMiddleware = require('../src/middlewares/not-found')
+const errorMiddleware = require('../src/middlewares/error')
+const rateLimitMiddleware = require('../src/middlewares/rate-limit')
+
 
 const app = express();
 
 app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
+
+app.use(rateLimitMiddleware)
+app.use(errorMiddleware)
+app.use(notFoundMiddleware)
 
 
 const PORT = process.env.PORT || "5000";
--- src/middlewares/not-found.js
+++ src/middlewares/not-found.js
@@ -1,3 +1,3 @@
-modulet.exports = (req, res, next) => {
+module.exports = (req, res, next) => {
   res.status(404).json({ message: "Resource Not Found on this Server" });
 };
