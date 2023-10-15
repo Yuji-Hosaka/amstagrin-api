9a5069ef323764fee24a8d710c410cb1b4df6167
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-15 14:14:09 +0700

    add feature update profileImage

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,6 +1,7 @@
 const prisma = require("../models/prisma");
-const createError = require('../utils/create-error')
-const {upload} = require('../utils/cloudinary-server')
+const createError = require("../utils/create-error");
+const { upload } = require("../utils/cloudinary-server");
+const fs = require("fs/promises");
 
 exports.updateProfile = async (req, res, next) => {
   try {
@@ -18,5 +19,9 @@ exports.updateProfile = async (req, res, next) => {
     res.status(200).json({ message: "UPLOAD SUCCESSFUL" });
   } catch (err) {
     next(err);
+  } finally {
+    if (req.files.profileImage) {
+      fs.unlink(req.files.profileImage[0].path);
+    }
   }
 };
