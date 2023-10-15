b48e483e9e9a158df230c7696f6721c7130daafe
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-10-15 14:59:22 +0700

    create func. getUserById, modified update profileImage

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -2,26 +2,53 @@ const prisma = require("../models/prisma");
 const createError = require("../utils/create-error");
 const { upload } = require("../utils/cloudinary-server");
 const fs = require("fs/promises");
+const { checkUserIdSchema } = require("../validators/user-validator");
+const { error } = require("console");
 
 exports.updateProfile = async (req, res, next) => {
   try {
     if (!req.files) {
       return next(createError("Profile image is required"));
     }
+
+    const response = {};
+
     if (req.files.profileImage) {
       const url = await upload(req.files.profileImage[0].path);
+      response.profileImage = url;
       await prisma.user.update({
         data: { profileImage: url },
         where: { id: req.user.id },
       });
     }
 
-    res.status(200).json({ message: "UPLOAD SUCCESSFUL" });
+    res.status(200).json(response);
   } catch (err) {
     next(err);
   } finally {
     if (req.files.profileImage) {
       fs.unlink(req.files.profileImage[0].path);
     }
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const { error } = checkUserIdSchema.validate(req.params);
+    if (error) {
+      return next(error);
+    }
+    const userId = +req.params.userId;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    if (user) {
+      delete user.password;
+    }
+    res.status(200).json({ user });
+  } catch (err) {
+    next(error);
   }
 };
